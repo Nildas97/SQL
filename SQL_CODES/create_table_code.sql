aa3c90891dc852c3aa957fd9691a7900bbda7fbc
Nilutpal Das <128306225+Nildas97@users.noreply.github.com>
2023-12-19 09:24:13 +0530

    Add files via upload

---
+++ SQL_CODES/create_table_code.sql
@@ -0,0 +1,8 @@
+CREATE TABLE customer 
+(
+	cust_id int PRIMARY KEY,
+	cust_name varchar(50) NOT NULL,
+	cust_age int NOT NULL,
+	cust_city char(50),
+	cust_salary numeric
+);
---
+++ SQL_CODES/insert_table_code.sql
@@ -0,0 +1,6 @@
+INSERT INTO customer (cust_id, cust_name, cust_age, cust_city, cust_salary)
+VALUES 
+(1, 'Ram', 28, 'Delhi', 9000),
+(2, 'Shyam', 27, 'Banglore', 8000),
+(3, 'Bharat', 26, 'Mumbai', 7000),
+(4, 'Shatrughn', 25, 'Kolkata', 6000);
